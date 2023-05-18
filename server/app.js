7742433e5453dbdacf101faa9aca9091608cd704
Your Name <your.email@example.com>
2023-05-18 15:00:42 +0530

    final commit2

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose=require('./Model/Mongoose')
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 require('dotenv').config()
  
 var app = express();
